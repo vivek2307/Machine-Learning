14a77fa8e59d4ffa0c05c530c70a30ab524222f0
Vivek Pandey <62239849+vivek2307@users.noreply.github.com>
2020-06-25 22:21:47 +0530

    Update RealTimeSentimentAnalysis.py

--- RealTimeSentimentAnalysis.py
+++ RealTimeSentimentAnalysis.py
@@ -11,10 +11,10 @@ class SentimentAnalysis:
 
     def DownloadData(self):
         # authenticating
-        consumerKey = 'nAfAiljzt8Ntd1oatjYU2uvKc'
-        consumerSecret = 'JBJahC7nKS4q853V0vodZ8buaYGHwySv6u5XFm52dlownOqTRm'
-        accessToken = '4880646753-8TzBk1UEjuvXEXR2XL7uJ2ZFMRZnwNrDOioUHI9'
-        accessTokenSecret = 'CjAas3cB7A9OW7SJFA6SOkyfgv61MyN59W0g47ZSqBL2b'
+        consumerKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        consumerSecret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        accessToken = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        accessTokenSecret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
         auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
         auth.set_access_token(accessToken, accessTokenSecret)
         api = tweepy.API(auth)
